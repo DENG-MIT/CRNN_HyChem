2e192d195da2d850f25350153bf82516bf294a7c
jiweiqi <jiweiqi10@gmail.com>
2021-02-19 21:59:50 -0500

    update display

--- network.jl
+++ network.jl
@@ -4,22 +4,28 @@ p[end] = 0.1;  # slope
 @inbounds function p2vec(p)
     slope = p[end] .* 10.0
     w_b = p[1:nr] .* slope
-    w_in_b = p[nr+1:nr*2]
-    w_in_Ea = p[nr*2+1:nr*3] .* slope
-    w_out = E_null' * reshape(p[nr*3+1:nr*(nse+3)], nse, nr)
+    w_in_b = p[nr + 1:nr * 2]
+    w_in_Ea = p[nr * 2 + 1:nr * 3] .* slope
+    w_out = E_null' * reshape(p[nr * 3 + 1:nr * (nse + 3)], nse, nr)
     w_in = vcat(clamp.(-w_out, 0.0, 4.0), w_in_Ea', w_in_b')
     return w_in, w_b, w_out
 end
 
 function display_p(p)
     w_in, w_b, w_out = p2vec(p)
-    println("\n species (column) reaction (row)")
-    println("w_in | Ea | b | logA")
-    show(stdout, "text/plain", round.(hcat(w_in', w_b), digits = 2))
-    println("\n w_out")
-    show(stdout, "text/plain", round.(w_out', digits = 2))
+    println("\n species (row) reaction (col)")
+    println(varnames[1:8])
+    println(varnames[9:end])
+    println("w_out | Ea | b | logA")
+    show(stdout, "text/plain", round.(hcat(w_out', w_in'[:, end - 1:end], w_b)', digits=2))
+    # println("\n w_out")
+    # show(stdout, "text/plain", round.(w_out', digits = 2))
+    println("\n w_nse")
+    w_e = reshape(p[nr * 3 + 1:nr * (nse + 3)], nse, nr)
+    show(stdout, "text/plain", round.(w_e, digits=2))
     println("\n")
 end
+display_p(p)
 
 @inbounds function crnn!(du, u, p, t)
     T::typeof(u[1]) = itpT(t)
@@ -45,18 +51,18 @@ prob = ODEProblem(crnn!, u0, tspan)
 
 ode_solver = TRBDF2();
 sense = ForwardDiffSensitivity()
-@inbounds function predict_n_ode(p, i_exp, sample = ntotal; dense = false)
+@inbounds function predict_n_ode(p, i_exp, sample=ntotal; dense=false)
     global w_in, w_b, w_out = p2vec(p)
     ylabel = @view(yall[i_exp, :, :])
     ts = @view(ylabel[1, :])
     global itpT =
-        LinearInterpolation(ts, @view(ylabel[2, :]); extrapolation_bc = Flat())
+        LinearInterpolation(ts, @view(ylabel[2, :]); extrapolation_bc=Flat())
     global itpP =
-        LinearInterpolation(ts, @view(ylabel[3, :]); extrapolation_bc = Flat())
+        LinearInterpolation(ts, @view(ylabel[3, :]); extrapolation_bc=Flat())
     u0 = zeros(gas.n_species)
     u0[ind_obs] .= @view(ylabel[4:end, 1])
     u0[ind_N2] = 1 - sum(u0)
-    _prob = remake(prob, u0 = u0, p = p, tspan = [0, ts[sample]])
+    _prob = remake(prob, u0=u0, p=p, tspan=[0, ts[sample]])
     if dense
         _ts = []
     else
@@ -65,17 +71,17 @@ sense = ForwardDiffSensitivity()
     sol = solve(
         _prob,
         ode_solver,
-        saveat = _ts,
-        atol = atol,
-        rtol = rtol,
-        sensalg = sense,
-        maxiters = maxiters,
+        saveat=_ts,
+        atol=atol,
+        rtol=rtol,
+        sensalg=sense,
+        maxiters=maxiters,
     )
     pred = Array(sol)
 end
-@time pred = predict_n_ode(p, 1; dense = false);
+@time pred = predict_n_ode(p, 1; dense=false);
 
-@inbounds function loss_n_ode(p, i_exp, sample = ntotal; get_ind = false)
+@inbounds function loss_n_ode(p, i_exp, sample=ntotal; get_ind=false)
     pred = clamp.(predict_n_ode(p, i_exp, sample), -ub, ub)[ind_obs, :]
     ind = size(pred)[2]
 
@@ -98,4 +104,4 @@ end
         return loss
     end
 end
-@time loss_n_ode(p, 1; get_ind = true)
+@time loss_n_ode(p, 1; get_ind=true)
